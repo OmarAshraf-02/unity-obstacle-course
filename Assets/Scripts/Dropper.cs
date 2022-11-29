1190dd74aca22715d9b137a565aca8975b754e68
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2022-11-30 01:55:27 +0200

    init

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    new MeshRenderer renderer;
+    new Rigidbody rigidbody;
+    [SerializeField] float timeToWait = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        renderer.enabled = false;
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timeToWait)
+        {
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
+        }
+
+        
+    }
+}
